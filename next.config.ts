08fcb93e90d0af49395b8758c83a0bbcc86887ec
Harish Malakar <harishmalakar19@gmail.com>
2025-01-20 20:06:36 +0530

    all files added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['res.cloudinary.com'], // Add Cloudinary's domain for loading images
+  },
+  env: {
+    CLOUDINARY_CLOUD_NAME: 'your-cloud-name', // Replace with your Cloudinary cloud name
+  },
+
 };
 
 export default nextConfig;
